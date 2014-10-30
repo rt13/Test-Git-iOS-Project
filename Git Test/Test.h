57d51118fcf4342686956ee59a9ef6334cc18b1e
Innoband <innoband@Innobands-MacBook-Air.local>
2014-10-30 14:09:06 -0700

    added test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Innoband on 10/30/14.
+//  Copyright (c) 2014 Bitfountain.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Innoband on 10/30/14.
+//  Copyright (c) 2014 Bitfountain.io. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
